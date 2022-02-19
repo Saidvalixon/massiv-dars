8ee723ef9c488bca253d8044bcfe8e3e9650afd5
Saidvalixon <99729716+Saidvalixon@users.noreply.github.com>
2022-02-19 12:04:26 +0500

    Add files via upload

---
+++ massiv dars/massiv/massiv.js
@@ -0,0 +1,36 @@
+myArray = ["Ford", "BMW", "Fiat"];
+
+// console.log(myArray)
+// console.log(myArray[1])
+// myArray[2] = "ismoil"
+// myArray[0] = "nizomiddin"
+// console.log(myArray)
+
+// said = ["said", "ismoil", "barat", "nizomiddin" ]
+// console.log( said[0] + " nma gap")
+
+// var autosalon = {
+// 	turi: "gentra ",
+// 	narxi: " 13800$ ",
+// 	joylawiw: "xamza",
+// 	rang: [ "qora", "oq", "qizil", "mat", "delfin"]
+// }
+// console.log(autosalon ['turi'] + autosalon ['rang'][0] + autosalon ['narxi'])
+
+var names = []
+barat: '2001'
+ismoil: '2001'
+nizomiddin: '2005'
+said: '2004'
+console.log(names ['barat'])
+
+
+
+
+// (
+// var barat ["oqiw"] = "MDIS"
+// var barat ["iw"] = "web universe"
+// var barat ["tel"] = "redmi"
+
+// console.log(barat["iw"])
+// )
---
+++ massiv dars/massiv2/dom.js
@@ -0,0 +1,135 @@
+// var x = document.getElementById('ismoil')
+// var y = document.getElementsByTagName('h1')
+// var z = document.getElementsByClassName('hello')
+// document.querySelector('nmadr')
+// document.querySelectorAll('.batrat'[2])
+// console.log(x)
+// console.log(y)
+
+
+// var x = document.getElementById('demo').innerHTML = "bolla nma gap"
+// var y = document.getElementsByClassName('free').innerHTML = "iwqilib ohwasin"
+// var z = document.getElementsByTagName('free').innerHTML = "iwqilib ohwasin"
+// var q = document.querySelector('h2').innerHTML = "ismoil nma gap"
+// var a = document.querySelectorAll('.barat'[2])
+// console.log(a)
+
+// var carSeller1 = {
+//   firstName: "Jane",
+//   lastName: "White",
+//   regYear: 2017,  
+//   hasDiscount: true, // Boolean
+//   discountCalculation: function(year){
+//     var discount;
+//     var numberOfYears = 2020;
+//     if((numberOfYears-year) <= 2){
+//       discount = 0;
+//     } else if((numberOfYears-year) >= 2 && (numberOfYears-year) <= 5){
+//       discount = 20;
+//     } else if((numberOfYears-year) > 5){
+//       discount = 30;
+//     }      
+//     return discount;
+//   }
+// }
+
+// console.log(carSeller1.discountCalculation(2016));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
